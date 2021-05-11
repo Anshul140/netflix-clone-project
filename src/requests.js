758a9c853567c3ba6494681337f7b302c9c8b79b
Anshul <anshulmahato61@gmail.com>
2021-05-11 12:25:38 +0530

    2nd commit- All components and Designing finished

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "1d6627887e663d738c7bb4004e43c0f5" ;
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  };
+  
+  export default requests;
